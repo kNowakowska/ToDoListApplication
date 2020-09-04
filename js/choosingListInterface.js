affaf2123639aa09a2a71d64fb8deb67515500d3
Kinga Nowakowska <k.nowakowska.013@gmail.com>
2020-09-04 11:02:00 +0200

    remove listDeleting.js and moving its content to choosingListInterface.js, fix problems: incorrect comparison in 41 line, adding li elements from not empty array, create mouseout event to hide deleting icon

--- js/choosingListInterface.js
+++ js/choosingListInterface.js
@@ -1,23 +1,21 @@
-let setOfLists = [];
-
-if(setOfLists.length>0){
-    for(let i=0; i<setOfLists.length; i++){
-        let el = '<li>'+setOfLists[i]+' <i class="fas fa-times-circle"></i></li>';
-        $(".fa-times-circle").hide();
-        $listsContainer.append(item);
-    }
-}
-
 $(function(){
+    let setOfLists = ["lista1", "lista5", "moja lista"];
 
     const $addListBtn = $('.add-list-btn');
     const $listsContainer = $(".panel ul");
     const $newListForm = $(".new-list-form");
     const $listNameInput = $(".list-name");
 
-
     $addListBtn.show();
     $newListForm.hide();
+
+    if(setOfLists.length>0){
+        for(let i=0; i<setOfLists.length; i++){
+            let item = '<li>'+setOfLists[i]+' <i class="fas fa-times-circle"></i></li>';
+            $(".fa-times-circle").hide();
+            $listsContainer.append(item);
+        }
+    }
 
     $addListBtn.on("click", function(){
         $newListForm.show();
@@ -25,7 +23,7 @@ $(function(){
 
     $newListForm.on("submit", function(e){
         e.preventDefault();
-        let name = $listNameInput.val();
+        let name = $listNameInput.val().trim();
         if(name){
             setOfLists.push(name);
             let item = '<li>'+name+' <i class="fas fa-times-circle"></i></li>';
@@ -36,13 +34,11 @@ $(function(){
             alert("Podaj nazwę listy!");
         }
         $newListForm.hide();
-        console.log(setOfLists)
     })
     
     function deleteElement(name, flag){
         for(let i=0; i<setOfLists.length && flag; i++){
-            if(setOfLists[i].toString()==name.toString()){
-                console.log("ZNALEZIONE")
+            if(setOfLists[i]==name){
                 setOfLists.splice(i,1);
                 flag=false;
             }
@@ -53,16 +49,17 @@ $(function(){
 
         $(this).children().show();
         $(this).on("click", "i", function(){
-            let name = $(this).parent().text();
-            console.log(name);
+            let name = $(this).parent().text().trim();
             $(this).parent().remove();
            deleteElement(name, true);
         })
-        console.log(setOfLists)
     })
+
+    $(".panel ul").on("mouseout","li", function(){
+        $(this).children().hide();
+    })
+   
    
 })
 
-
-//<i class="fas fa-times-circle"></i> // icon to delete list from the panel
 
